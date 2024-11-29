041463697a6725aa861a0e67369fca1c99f41ce9
CeciFerrer <g.ceciliaferrer@gmail.com>
2024-11-29 20:07:10 +0100

    hasta capitulo 37 del curso

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,13 +8,22 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required:true}) avatar!: string;
+  @Input({required:true}) id!:string;
+  @Input({required:true}) avatar!: string;  //el signo "!" se pone porque el string no esta inicializado
   @Input({required:true}) name!: string;
+  @Output() select = new EventEmitter<string>();
+  //select = output<string>();  // NO es una signal !!
+  //avatar = input.required<string>();  //son signals
+  //name = input.required<string>();
 
+  //imagePath = computed(()=>{   //signal
+  //  return '../assets/users/' + this.avatar;
+  // });
   get imagePath(){
     return '../assets/users/' + this.avatar;
   }
-  onSelectUser(){
-    
-  }
+
+  onSelectUser(id:string){
+    this.select.emit(this.id);
+   }
 }
