644364350c27222fd74f10996fc3164d26619f0a
CeciFerrer <g.ceciliaferrer@gmail.com>
2024-12-01 10:49:02 +0100

    hasta clase 46

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
-import { NgFor, NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,42 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-
+  @Input({required :true}) userId!: string;
  // @Input() name?: string;
- @Input() name: string | undefined;   //otra alternativa
+ @Input({required :true}) name!: string;   //otra alternativa
+ tasks = [
+  {
+    id:'t1',
+    userId: 'u1',
+    title: 'Master Angular',
+    summary: 'Learn all the bsico and advanced featuras of Angular & how to apply them',
+    dueDate: '2025-12-31'
+  },
+  {
+    id:'t2',
+    userId: 'u2',
+    title: 'Master Angular',
+    summary: 'Learn all the bsico and advanced featuras of Angular & how to apply them',
+    dueDate: '2025-12-31'
+  },
+  {
+    id:'t3',
+    userId: 'u3',
+    title: 'Master Angular',
+    summary: 'Learn all the bsico and advanced featuras of Angular & how to apply them',
+    dueDate: '2025-12-31'
+  },
+ ];
+
+ get selectedUserTasks(){
+  return this.tasks.filter((task)=>task.userId === this.userId);
+ }
 }
