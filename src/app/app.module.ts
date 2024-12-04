fb7d45a1f9a8c1bd547b3dd20544aecec1421928
CeciFerrer <g.ceciliaferrer@gmail.com>
2024-12-04 13:25:53 +0100

    clase 73, finalizamos migracion a modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
+
 
 @NgModule({
-    declarations: [AppComponent], //non standalone comp
+    declarations: [AppComponent, HeaderComponent, UserComponent], //non standalone comp
     bootstrap: [AppComponent],
-    imports: [HeaderComponent, UserComponent, TasksComponent,BrowserModule] // for standalone comp
+    imports: [BrowserModule, SharedModule, TasksModule ] // for standalone comp / DatePipe esta incluido en BrowserModule
+    //tambien para importar otros Modules
 })
-export class AppModule{
-
-}
+export class AppModule{}
